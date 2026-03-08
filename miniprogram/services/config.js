4db45a10f691375abfd6f80029d18fb717490258
liqqi7 <liqqi@live.com>
2026-03-08 17:24:09 +0800

    chore: pin miniprogram api base url to production

--- miniprogram/services/config.js
+++ miniprogram/services/config.js
@@ -1,24 +1,10 @@
-const LOCAL_API_BASE_URL = "http://127.0.0.1:8000/api/v1";
-const PRODUCTION_API_BASE_URL = "https://dragon.liqqihome.top/api/v1";
-
-function getEnvVersion() {
-  try {
-    const info = wx.getAccountInfoSync && wx.getAccountInfoSync();
-    return info && info.miniProgram ? info.miniProgram.envVersion : "develop";
-  } catch (err) {
-    return "develop";
-  }
-}
+const API_BASE_URL = "https://dragon.liqqihome.top/api/v1";
 
 function getApiBaseUrl() {
-  const customApiBaseUrl = wx.getStorageSync("apiBaseUrl");
-  if (customApiBaseUrl) return customApiBaseUrl;
-
-  return getEnvVersion() === "develop" ? LOCAL_API_BASE_URL : PRODUCTION_API_BASE_URL;
+  return API_BASE_URL;
 }
 
 module.exports = {
-  LOCAL_API_BASE_URL,
-  PRODUCTION_API_BASE_URL,
+  API_BASE_URL,
   getApiBaseUrl
 };
