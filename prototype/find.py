74249a1c6132adb8ca92a8e19da39efc83e5cd6a
Novusluna <258411286+Novusluna66@users.noreply.github.com>
2026-09-21 14:04:31 +0800

    chore: clean unused prototype assets

--- prototype/find.py
+++
@@ -1,7 +0,0 @@
-﻿import json
-p='龙城小程序.pen'
-with open(p,encoding='utf-8') as f: d=json.load(f)
-def walk(n):
- if n.get('id')=='Itazg': print(json.dumps(n,ensure_ascii=False,indent=2))
- for c in n.get('children',[]): walk(c)
-walk(d)
